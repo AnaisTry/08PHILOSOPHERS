a78bc5c8c61ef1712c50dac20d837b2a86336ab2
Anais Gassin <angassin@student.s19.be>
2023-05-25 19:21:37 +0200

    pthread.c

---
+++ thread.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+
+void *routine() {
+    printf("Hello from threads\n");
+    sleep(3);
+    printf("Ending thread\n");
+}
+
+int main(int argc, char* argv[]) {
+    pthread_t p1, p2;
+    if (pthread_create(&p1, NULL, &routine, NULL) != 0) //pointer to pthread, attributes (customisation), pointer to function, arguments of function
+    {
+        return 1;
+    }
+    if (pthread_create(&p2, NULL, &routine, NULL) != 0) {
+        return 2;
+    }
+    if (pthread_join(p1, NULL) != 0) // = wait() for threads : pthread struct, pointer that store the return
+    {
+        return 3;
+    }
+    if (pthread_join(p2, NULL) != 0) {
+        return 4;
+    }
+    return 0;
+}
