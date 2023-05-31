389cf2d08c95ac268534fb54ff7109b402992b36
Anais Gassin <angassin@student.s19.be>
2023-05-31 10:23:49 +0200

    malloc philos and forks

--- philo/init.c
+++ philo/init.c
@@ -6,7 +6,7 @@
 /*   By: angassin <angassin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/30 17:02:29 by angassin          #+#    #+#             */
-/*   Updated: 2023/05/31 10:18:12 by angassin         ###   ########.fr       */
+/*   Updated: 2023/05/31 10:23:31 by angassin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int	symposium_init(int argc, char **argv, t_symposium *s)
 {
 	s->nb_philo = ft_atoi(argv[1]);
 	s->philos = malloc(sizeof(s->philos) * s->nb_philo);
-	s->forks = malloc(sizeof(s->philos) * s->nb_philo);
+	s->forks = malloc(sizeof(s->forks) * s->nb_philo);
 	s->time_to_die = ft_atoi(argv[2]);
 	s->time_to_eat = ft_atoi(argv[3]);
 	s->time_to_sleep = ft_atoi(argv[4]);
